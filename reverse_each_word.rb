29aa77de0397b182f55d0a10e607f417ccfd684d
Denise Pendleton <dpendl1@hotmail.com>
2018-02-21 05:35:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(sentence)
   # sentence = "Hello, this is a test"
   array = sentence.split
   new_array = []
-  array.each do |element| 
+  array.each do |element|
     new_array << element.reverse
   end
   new_array.join (" ")
- 
+
 end
