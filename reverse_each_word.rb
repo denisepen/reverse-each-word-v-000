1ba0f4f70094b6177ac37af70ede8d6c5aa87e56
Denise Pendleton <dpendl1@hotmail.com>
2018-02-21 05:35:16 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence)
+  # sentence = "Hello, this is a test"
+  array = sentence.split
+  new_array = []
+  array.each do |element| 
+    new_array << element.reverse
+  end
+  new_array.join (" ")
+ 
+end
